76ca27e5a504f55e5f28e31ff8421d238760b381
mgalindo077 <mgalindo077@gmail.com>
2020-01-06 14:07:04 -0600

    'Starting'

---
+++ calculos_generales.py
@@ -0,0 +1,17 @@
+def sumar(op1, op2):
+	print("El resultado de la suma es: ", op1+op2)
+
+def restar(op1, op2):
+	print("El resultado de la resta es: ", op1-op2)
+
+def multiplicar(op1,op2):
+	print("El resultado de la multiplicacion es: ",op1*op2)
+
+def dividir(op1,op2):
+	print("El resultado de la division es: ", op1/op2)
+
+def potencia(base, exponente):
+	print("El resultado de elevar el numero es: ",base**exponente)
+
+def redondear(op1,dec):
+	print("El resultado redondeado es: ", round(op1,dec))
---
+++ redondeo_potencia/redondeoYpotencia.py
@@ -0,0 +1,5 @@
+def potencia(base, exponente):
+	print("El resultado de elevar el numero es: ",base**exponente)
+
+def redondear(op1,dec):
+	print("El resultado redondeado es: ", round(op1,dec))
